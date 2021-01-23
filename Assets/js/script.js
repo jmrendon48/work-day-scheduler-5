912bd6e6b5804cd652cc957ac92ac8045af46267
AlexDeLarge <jmrendon48@gmail.com>
2021-01-22 18:38:19 -0800

    added ability to add and edit events

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,7 @@ var currentDayUpdate = function() {
     $("#currentDay").text(currentDay);
 };
 
+// checks the hour and color codes time blocks accordingly
 var timeBlockColorCoding = function() {
     // get current hour of the day
     var currentHour = moment().hour();
@@ -15,12 +16,11 @@ var timeBlockColorCoding = function() {
         // get the hour of each div, and turn it into an integer
         var timeBlockContainer = $(".event-container").filter('[data-hour=' + [i] + "]");
         timeBlockHour = timeBlockContainer.data("hour");
-        console.log(timeBlockHour);
         timeBlockHour = parseInt(timeBlockHour);
 
-        //
+        // adds appropriate class depending on what hour it is
         if (timeBlockHour < currentHour) {
-            timeBlockContainer.addClass("past");
+            timeBlockContainer.addClass("past");  
         } else if (timeBlockHour === currentHour) {
             timeBlockContainer.addClass("present");
         } else {
@@ -29,6 +29,36 @@ var timeBlockColorCoding = function() {
     }
 };
 
+$(".event-container").on("click", "p", function() {
+    // get current text of p element
+    var text = $(this)
+        .text()
+        .trim();
+
+    // replace p element with a new textarea
+    var textInput = $("<textarea>")
+        .addClass("event-textarea")
+        .val(text);
+    $(this).replaceWith(textInput);
+
+    // auto focus new element
+    textInput.trigger("focus");
+});
+
+$(".event-container").on("blur", "textarea", function() {
+    // get current value of textarea
+    var text = $(this).val();
+
+    var eventP = $("<p>")
+        .addClass("event")
+        .text(text);
+
+    // replace textarea with new content
+    $(this).replaceWith(eventP);
+});
+
 currentDayUpdate();
 
-setInterval(timeBlockColorCoding(), 300000);
+timeBlockColorCoding();
+
+setInterval(timeBlockColorCoding, 60000);
