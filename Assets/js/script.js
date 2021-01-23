4745678cd8d048035a0da2ab3b1ae5a213147321
AlexDeLarge <jmrendon48@gmail.com>
2021-01-22 16:01:41 -0800

    added color coding to time blocks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,4 +4,31 @@ var currentDayUpdate = function() {
     $("#currentDay").text(currentDay);
 };
 
+var timeBlockColorCoding = function() {
+    // get current hour of the day
+    var currentHour = moment().hour();
+
+    // remove old time block background-color class
+    $(".event-container").removeClass("past present future");
+
+    for (var i = 9; i < 18; i++) {
+        // get the hour of each div, and turn it into an integer
+        var timeBlockContainer = $(".event-container").filter('[data-hour=' + [i] + "]");
+        timeBlockHour = timeBlockContainer.data("hour");
+        console.log(timeBlockHour);
+        timeBlockHour = parseInt(timeBlockHour);
+
+        //
+        if (timeBlockHour < currentHour) {
+            timeBlockContainer.addClass("past");
+        } else if (timeBlockHour === currentHour) {
+            timeBlockContainer.addClass("present");
+        } else {
+            timeBlockContainer.addClass("future");
+        }
+    }
+};
+
 currentDayUpdate();
+
+setInterval(timeBlockColorCoding(), 300000);
